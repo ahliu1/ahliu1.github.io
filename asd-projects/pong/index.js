3d5b2704a62cd2ec5bcb6e94ff3edceaadfa3c9e
ahliu1 <Aliu23@bfhsla.org>
2021-06-16 21:31:00 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -11,6 +11,8 @@ function runProgram(){
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   var KEY = {
+      //starts game when space bar is clicked (to ensure players are ready)
+      "SPACE" : 32,
       //player 1 keys
       "W-UP" : 87, 
       "S-DOWN" : 83,
@@ -23,27 +25,32 @@ function runProgram(){
   var boardWidth = $("#board").width();
   var boardHeight = $("#board").height();
 
-  //variables for abstraction
-  var leftPaddleHeight = $("#leftPaddle").height();
-  var rightPaddleHeight = $("#rightPaddle").height();
-
   // Game Item Objects
 
   //players' paddles
-  var leftPaddle = gameObject("#leftPaddle", 1, 325, 0, 0);
-  var rightPaddle = gameObject("#rightPaddle", 1404, 325, 0, 0);
+  var leftPaddle = gameObject("#leftPaddle");
+  var rightPaddle = gameObject("#rightPaddle");
+  var leftPaddleHeight = $("#leftPaddle").height();
+  var rightPaddleHeight = $("#rightPaddle").height();
   
   //pong ball
-  var ball = gameObject("#ball", 707, 340, Math.random() * 8, Math.random() * 8);
+  var ball = gameObject("#ball");
+  var ballStartingX = 707; 
+  var ballStartingY = 340;
 
   //player scores
-  var leftPlayerScore = gameObject("#leftPlayerScore", 0, 0, 0, 0);
-  var rightPlayerScore = gameObject("#rightPlayerScore", 0, 0, 0, 0);
+  var leftPlayerScore = gameObject("#leftPlayerScore");
+  var rightPlayerScore = gameObject("#rightPlayerScore");
+  var leftPaddleScore = 0;
+  var rightPaddleScore = 0;
+  $("#leftPlayerScore").text(leftPaddleScore);
+  $("#rightPlayerScore").text(rightPaddleScore);
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
+
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -65,7 +72,10 @@ function runProgram(){
     repositionBall();
     redrawBall();
 
-    //prevents paddle from leaving board
+    //stops and resets ball to original position, helps tally points
+    ballStop();
+
+    //prevents paddles from leaving board
     if (leftPaddle.y >= boardHeight - leftPaddleHeight){
         leftPaddle.speedY = -leftPaddle.speedY;
      } else  if (leftPaddle.y <= 0){
@@ -77,28 +87,39 @@ function runProgram(){
      } else  if (rightPaddle.y <= 0){
         rightPaddle.speedY = -rightPaddle.speedY;
   }
-    //prevents ball from leaving board
-    if (ball.y >= boardHeight){
-        ball.speedY = -ball.speedY;
-    } else if (ball.y <= 0){
-        ball.speedY = -ball.speedY;
+    // uses collision function to bounce ball off of paddles
+    if (doCollide(leftPaddle, ball)){
+         ball.speedX = -ball.speedX;
     }
+     if (doCollide(rightPaddle, ball)){
+         ball.speedX = -ball.speedX;
+     }
 
+     // ends the game when either player has 11 points
+    if (leftPaddleScore >= 11 || rightPaddleScore >= 11){
+        endGame();
+    }
   }
-
 
   /* 
   Called in response to keydown events.
   */
   
   function handleKeyDown(event){
-      //handles key-down events for left player, allows paddle to move
+    //starts game when the space bar is pressed to ensure both players are ready
+      if (event.which === KEY.SPACE){
+        randomStart(); 
+        $("#alert").remove();
+      }
+
+      //handles key-down events for left player, allows left paddle to move
       if (event.which === KEY["S-DOWN"]){
           leftPaddle.speedY = 5;
       } else if (event.which === KEY["W-UP"]){
           leftPaddle.speedY = -5;
       }
-      //handles key-down events for right player, allows paddle to move
+
+      //handles key-down events for right player, allows right paddle to move
       if (event.which === KEY.DOWN){
           rightPaddle.speedY = 5;
       } else if (event.which === KEY.UP){
@@ -124,18 +145,26 @@ function runProgram(){
       }
   }
 
+
+
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  function gameObject($id, x, y, speedX, speedY){
-    var object = {};
-    object.id = $id;
-    object.x = x;
-    object.y = y;
-    object.speedX = speedX;
-    object.speedY = speedY;
-    return object;
-} //function for abstraction of our game objects
+  
+  //factory function for our game objects to avoid repetition 
+  function gameObject($id){
+    var Object = {};
+  
+    Object.id = $id;
+    Object.x = Number($($id).css('left').replace(/[^-\d\.]/g, ''));
+    Object.y = Number($($id).css('top').replace(/[^-\d\.]/g, ''));
+    Object.width = $($id).width();
+    Object.height = $($id).height();
+    Object.speedX = 0;
+    Object.speedY = 0;
+
+    return Object;
+} 
 
 //helper functions for updating left player's paddle
 function repositionLeftPaddle(){
@@ -155,6 +184,34 @@ function redrawRightPaddle(){
     $("#rightPaddle").css("top", rightPaddle.y);
 }
 
+
+//helper functions to help randomize the ball's starting process
+
+// helper function to use later in randomStart(); ensures that probability is equal and start is randomized
+function randomNumber(number) {	
+    var num = Math.ceil(Math.random() * number);
+    return num;
+}	
+
+function randomStart(){
+    // determines ball's horizontal direction
+    var startX = randomNumber(2); // picking a random number b/w 1 or 2 so probability is equal
+    if (startX % 2 === 0){ // if randomNumber is 2, ball goes to left first
+        ball.speedX = -10;
+    }
+    else if (startX % 2 === 1){ //if randomNumber is 1, ball goes to right first
+        ball.speedX = 10;
+    }
+
+    // determines the ball's vertical direction
+    var startY = randomNumber(2);
+    if (startY % 2 === 0){
+        ball.speedY = -10; //if randomNumber is 2, ball goes up first
+    } else if (startY % 2 === 1){
+        ball.speedY = 10; //if randomNumber is 1, ball goes down first
+    }
+}
+
 //helper functions for updating ball's position
 function repositionBall(){
     ball.y += ball.speedY;
@@ -165,13 +222,73 @@ function redrawBall(){
     $("#ball").css("top", ball.y);
     $("#ball").css("left", ball.x);
 }
-  
+
+//helper function for stopping the ball, resetting its position, and tallying points
+
+function ballStop(){
+    // handles collisions for top and bottom walls, causes ball to bounce back
+    if (ball.y >= boardHeight){
+        ball.speedY = -ball.speedY;
+    } else if (ball.y <= 0){
+        ball.speedY = -ball.speedY;
+    } 
+    // handles collisions for left wall
+    else if (ball.x <= 0){ 
+        ball.speedY = 0; // stops ball movement
+        ball.speedX = 0;
+        ball.y = ballStartingY; // resets ball to starting position
+        ball.x = ballStartingX;
+        rightPaddleScore++; // increases and tallies right player's score
+        $("#rightPlayerScore").text(rightPaddleScore);
+    } 
+    // handles collisions for right wall
+    else if (ball.x >= boardWidth){ 
+        ball.speedY = 0; // stops ball movement
+        ball.speedX = 0;
+        ball.y = ballStartingY; //resets ball to starting position
+        ball.x = ballStartingX;
+        leftPaddleScore++; // increases and tallies left player's score
+        $("#leftPlayerScore").text(leftPaddleScore);
+    }
+}
+
+function doCollide(paddle, ball) { // helps register ball-paddle collisions
+    //sides of paddles
+    paddle.leftX = paddle.x;
+    paddle.topY = paddle.y;
+    paddle.rightX = paddle.x + paddle.width;
+    paddle.bottomY = paddle.y + paddle.height;
+
+    //sides of ball
+    ball.leftX = ball.x;
+    ball.topY = ball.y
+    ball.rightX = ball.x + ball.width;
+    ball.bottomY = ball.y + ball.height;
+
+    // conditional to register collision
+    if (ball.leftX < paddle.rightX && 
+        ball.rightX > paddle.leftX && 
+        ball.topY < paddle.bottomY && 
+        ball.bottomY > paddle.topY) {
+            return true;
+    } else {
+        return false;
+    }
+
+}
+
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
 
     // turn off event handlers
     $(document).off();
+    if (leftPaddleScore === 11){
+        $("#endingScreen").text("PLAYER ONE  VICTORY");
+    } else if (rightPaddleScore === 11){
+        $("#endingScreen").text("PLAYER TWO  VICTORY");
+    }
   }
   
 }
